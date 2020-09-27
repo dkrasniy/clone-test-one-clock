a2b88c1041fad37868998c481240fb93ff01299c
David Krasniy <davidkrasniy@gmail.com>
2020-09-27 15:20:52 -0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,12 @@ module.exports = {
     './src/**/*.jsx',
     './src/**/*.js'],
   },
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   theme: {
-    extend: {
-      colors: {
-        red: {
-          '100': '#ebf5fe',
-          '200': '#9dcefb',
-          '300': '#3a9cf8',
-          '400': '#0981f1',
-          '500': '#0874d9',
-          '600': '#0767c0',
-          '700': '#0657a2',
-          '800': '#0657a2',
-          '900': '#032d54',
-        }, 
-      },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [],
